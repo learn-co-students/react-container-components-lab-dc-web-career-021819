0ed25fa7f50323f8af28a9310dfbf59119d8d772
github username <github email address>
2019-04-26 13:09:58 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,37 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = 'c0s2A2QRmzH7TwJy8IatANqIFoHXFAjz';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+//fetch a list of the most recent reviews and display them
+class LatestMovieReviewsContainer extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentDidMount(){
+    fetch(URL)
+      .then(res => res.json())
+      .then(reviews => {
+        console.log(reviews.results)
+        this.setState({ reviews: reviews.results })
+      })
+  }
+
+  render(){
+    return (
+      <div className="latest-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
+
+export default LatestMovieReviewsContainer
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,52 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'c0s2A2QRmzH7TwJy8IatANqIFoHXFAjz';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?';
 
 // Code SearchableMovieReviewsContainer Here
+//will provide a searchable interface allowing the user to enter a search term and then receive a list of
+//reviews that match the search terms
+class SearchableMovieReviewsContainer extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      reviews: [],
+      searchTerm: ""
+    };
+  }
+
+  // componentDidMount(){
+  //   fetch(URL)
+  //     .then(res => res.json())
+  //     .then(reviews => this.setState({ reviews }))
+  // }
+  onChangeSearchEventHandler = (event) => {
+    this.setState({searchTerm: event.target.value})
+  }
+
+  onSubmitEventHandler = (event) => {
+    event.preventDefault()
+    fetch(URL + `query=<${this.state.searchTerm}>&api-key=${NYT_API_KEY}`)
+      .then(res => res.json())
+      .then(reviews => {
+        console.log(reviews.results);
+        this.setState({ reviews: reviews.results })
+      })
+  }
+
+  render(){
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.onSubmitEventHandler}>
+          <input type="text" onChange={this.onChangeSearchEventHandler}/>
+          <button type="submit">Search</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer
